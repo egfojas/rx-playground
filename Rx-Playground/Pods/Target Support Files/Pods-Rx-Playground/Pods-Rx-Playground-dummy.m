8b21e8d63173ca751f660de642458d1a3dd95640
Edgar Allan Fojas <edgar@circles.asia>
2019-09-16 13:58:34 +0800

    initial setup

---
+++ Rx-Playground/Pods/Target Support Files/Pods-Rx-Playground/Pods-Rx-Playground-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rx_Playground : NSObject
+@end
+@implementation PodsDummy_Pods_Rx_Playground
+@end
---
+++ Rx-Playground/Rx-Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rx-Playground
+//
+//  Created by Edgar Allan Fojas on 16/9/19.
+//  Copyright © 2019 Edgar Allan Fojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
