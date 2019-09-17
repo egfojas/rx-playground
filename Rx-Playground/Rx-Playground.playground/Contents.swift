7b1517c25fd271ba0898d42a94ced07ebc1aac59
Edgar Allan Fojas <edgar@circles.asia>
2019-09-17 14:23:31 +0800

    hot and cold

--- Rx-Playground/Rx-Playground.playground/Contents.swift
+++ Rx-Playground/Rx-Playground.playground/Contents.swift
@@ -2,3 +2,35 @@ import UIKit
 import RxSwift
 
 var str = "Hello, playground"
+let publisher = PublishSubject<Float>()
+
+publisher.subscribe(onNext: { (value) in
+    print("radio1: \(value)")
+})
+
+publisher.onNext(1)
+publisher.onNext(2)
+publisher.onNext(3)
+publisher.onNext(4)
+
+publisher.subscribe(onNext: { (value) in
+    print("radio2: \(value)")
+})
+
+publisher.onNext(5)
+publisher.onNext(6)
+
+let coldObservable = Observable.deferred({ () -> Observable<Date> in
+    return Observable.just(Date())
+})
+
+coldObservable.subscribe(onNext: { (value) in
+    print("radio3: \(value)")
+})
+
+sleep(3)
+
+coldObservable.subscribe(onNext: { (value) in
+    print("radio3: \(value)")
+})
+
